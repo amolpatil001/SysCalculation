bcc4556b81e258a22d94217428b486415413ca7a
Amol Patil <amolrpatil001@yahoo.com>
2018-10-12 15:09:20 +0530

    Initial commit

---
+++ SysCalculation/Controllers/CalculatorController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SysCalculation.Models;
+
+namespace SysCalculation.Controllers
+{
+
+
+//Create a simple MVC application that takes 2 numbers for input and returns the sum of the two numbers after the user clicks a button that says “Add”.  The platform should be.NET 4.7.2 or.NET Core 2.1 Either one is fine.
+
+//You can create a repository on Git (with your name) and upload the project there.If you compress it, please use ZIP format.
+    public class CalculatorController : Controller
+    {
+        // GET: Calculator
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Result(ModelAddition modelBinders)
+        {
+            ViewBag.result = modelBinders.firstValue + modelBinders.secondValue;
+          
+            return View("~/Views/Calculator/Index.cshtml");
+            //return JsonResult();
+        }
+
+
+    }
+}
---
+++ SysCalculation/Models/ModelAddition.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+
+namespace SysCalculation.Models
+{
+    public class ModelAddition
+    {
+        [Required]
+        public double firstValue { get; set; }
+
+        [Required]
+        public double secondValue { get; set; }
+        public string total { get; set; }
+    }
+}
